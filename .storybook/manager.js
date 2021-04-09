b1e7ead0db88c803dc7300bd8b33ab74c285a0ee
Stefan Zülch <stefan.zuelch@hr.de>
2021-04-09 11:35:49 +0200

    Adds two simple custom themes, HRDesignsystemLight and HRDesignsystemDark. At the moment the just inherit the styles from the default storybook light and dark themes and set the brandTitle to HR Designsystem.

---
+++ .storybook/manager.js
@@ -0,0 +1,19 @@
+import { addons } from "@storybook/addons";
+import { themes } from "@storybook/theming";
+import hrDesignsystemLight from "./HRDesignsystemLight";
+
+addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: "bottom",
+  enableShortcuts: true,
+  isToolshown: true,
+  theme: hrDesignsystemLight,
+  selectedPanel: undefined,
+  initialActive: "sidebar",
+  sidebar: {
+    showRoots: false,
+    collapsedRoots: ["other"],
+  },
+});
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
 import "../src/css/index.css";
+import { themes } from "@storybook/theming";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  docs: {
+    theme: themes.light,
+  },
 };
