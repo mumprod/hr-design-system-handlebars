b452b0dce22a2f6fb55fb9a08ae70af38fcc7e2a
Stefan Zülch <stefan.zuelch@hr.de>
2021-04-21 15:48:02 +0200

    use a custom defaultExtractor for purgecss to make sure that all css classes in a .hbs file are found

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./src/**/*.hbs"],
+  purge: {
+    content: ["./src/**/*.hbs"],
+    options: {
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
