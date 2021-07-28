38576254f177b44dacc20b0a0789c33531a4b40f
Stefan Zülch <stefan.zuelch@hr.de>
2021-07-28 15:52:59 +0200

    storybook-addon-css-variables-theme works with postcss

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,15 @@
 import "../src/assets/css/index.css";
 import "../src/assets/css/tailwind.css";
-//import "../src/assets/css/tailwind.hessenschau.css";
+
 import { themes } from "@storybook/theming";
 import hrDesignsystemDark from "./HRDesignsystemDark";
 import hrDesignsystemLight from "./HRDesignsystemLight";
 
-import hessenschau from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/assets/css/tailwind.hessenschau.css'
-import hr3 from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/assets/css/tailwind.hr3.css'
+import hessenschau from "!!style-loader?injectType=lazyStyleTag!css-loader!postcss-loader?{'postcssOptions': {'plugins': ['tailwindcss', 'autoprefixer']}}!../src/assets/css/tailwind.hessenschau.css";
+import hr3 from "!!style-loader?injectType=lazyStyleTag!css-loader!postcss-loader?{'postcssOptions': {'plugins': ['tailwindcss', 'autoprefixer']}}!../src/assets/css/tailwind.hr3.css";
 
-import cssVariablesTheme from '@etchteam/storybook-addon-css-variables-theme'
-
+import cssVariablesTheme from "@etchteam/storybook-addon-css-variables-theme";
+import postcss from "postcss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -27,13 +27,11 @@ export const parameters = {
     files: {
       hessenschau,
       hr3,
-    }
-  }
+    },
+  },
 };
 
 export const decorators = [
-  (Story) => (
-    '<div class="max-w-screen-lg mx-auto">' + Story() + '</div>'
-  ),
-  cssVariablesTheme
+  (Story) => '<div class="max-w-screen-lg mx-auto">' + Story() + "</div>",
+  cssVariablesTheme,
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: {
@@ -10,41 +10,40 @@ module.exports = {
   },
   theme: {
     letterSpacing: {
-      tighter: '-.025em',
-      tight: '-.0125em',
-      normal: '0',
-      wide: '.0125em',
-      wider: '.025em',
-      widest: '.05em',
-
+      tighter: "-.025em",
+      tight: "-.0125em",
+      normal: "0",
+      wide: ".0125em",
+      wider: ".025em",
+      widest: ".05em",
     },
     maxWidth: {
-      '1/4': '25%',
-      '1/3': '33.33333333%',
-      '1/2': '50%',
-      '2/3': '66.66666666%',
-      '3/4': '75%',
-      '1/1': '100%',
-     },
+      "1/4": "25%",
+      "1/3": "33.33333333%",
+      "1/2": "50%",
+      "2/3": "66.66666666%",
+      "3/4": "75%",
+      "1/1": "100%",
+    },
     extend: {
       fontFamily: {
-        copy:         ["DIN", "sans-serif"],
+        copy: ["DIN", "sans-serif"],
         headingSerif: ["RobotoSlab", "serif"],
-        heading:      ["RobotoCond", "sans-serif"],
+        heading: ["RobotoCond", "sans-serif"],
       },
       colors: {
         blue: {
-          science: '#006dc1'
-        }, 
+          science: "#006dc1",
+        },
         black: {
-          DEFAULT:'#000000'
+          DEFAULT: "#000000",
         },
         red: {
-          thunderbird: '#cc1a14'
+          thunderbird: "#cc1a14",
         },
-        labelMedia: 'var(--color-label-media)',
-        labelEvent: 'var(--color-label-event)',
-        labelBreakingnews: 'var(--color-label-breakingnews)'      
+        labelMedia: "var(--color-label-media)",
+        labelEvent: "var(--color-label-event)",
+        labelBreakingnews: "var(--color-label-breakingnews)",
       },
     },
   },
