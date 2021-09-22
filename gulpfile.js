faea97b49fb0a1c6a626068b835430ff7f714008
Stefan Zülch <stefan.zuelch@hr.de>
2021-09-22 16:21:19 +0200

    svgmap is created like in the delivery

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,61 @@ const glob = require('glob')
 const svgStore = require('gulp-svgstore')
 const svgMin = require('gulp-svgmin')
 const path = require('path')
+const cheerio = require('gulp-cheerio')
+const rename = require('gulp-rename')
 
 // css file paths
 const iconsDirRoot = 'src/assets/icons'
 
+const iconFoldersToCleanUp = ['icons', 'logo']
+const svgmapFilename = 'svgmap.min.svg'
+
 function svgMap() {
     return mergeStream(
         glob.sync(`${iconsDirRoot}/*`).map(function (iconsDir) {
-            console.log(iconsDir)
             let icon = path.basename(iconsDir)
             return src(`${iconsDir}/svgmap/*.svg`)
-                .pipe(svgMin())
-                .pipe(svgStore())
+                .pipe(
+                    svgMin({
+                        plugins: [
+                            {
+                                name: 'removeViewBox',
+                                active: false,
+                            },
+                            {
+                                name: 'inlineStyles',
+                                active: false,
+                            },
+                            {
+                                name: 'removeUnknownsAndDefaults',
+                                active: false,
+                            },
+                        ],
+                    })
+                )
+                .pipe(svgStore({ inlineSvg: true }))
+                .pipe(
+                    cheerio({
+                        run: function ($, file) {
+                            $('svg > symbol').attr('preserveAspectRatio', 'xMidYMid meet')
+                            $('[fill]').map(function () {
+                                if (
+                                    $(this).attr('fill') !== 'currentColor' &&
+                                    iconFoldersToCleanUp.includes(icon)
+                                ) {
+                                    $(this).removeAttr('fill')
+                                }
+                            })
+                            $('[preserve--fill]').map(function () {
+                                let value = $(this).attr('preserve--fill')
+                                $(this).removeAttr('preserve--fill')
+                                $(this).attr('fill', value)
+                            })
+                        },
+                        parserOptions: { xmlMode: true },
+                    })
+                )
+                .pipe(rename(svgmapFilename))
                 .pipe(dest(`${iconsDirRoot}/${icon}`))
         })
     )
