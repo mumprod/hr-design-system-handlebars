2c4fefb8f9c9a439fd15cdebb5573952d1915a06
Gerald Ohl <gerald.ohl@hr.de>
2021-10-22 10:27:03 +0200

    fetch-url for search-suggestions per resourceUrl

--- build/helpers/resourceUrl.js
+++ build/helpers/resourceUrl.js
@@ -5,6 +5,8 @@ module.exports = function(text, options) {
         resourceUrl = text.replace('assets/base/','./')
     if(text.includes('suche/index.nocache'))
         resourceUrl = text.replace('suche/index.nocache','suche/index.nc.html')
+    if(text.includes('suche/index~suggest.jsp'))
+        resourceUrl = text.replace('suche/index~suggest.jsp','https://hessenschau-dev-red.hr.de/suche/index~suggest.jsp')
 
     return resourceUrl
 
