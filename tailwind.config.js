d483ea397d940ddf1ff5bc3cf467b097047e9441
svesper <stefan.vesper@hr.de>
2021-10-26 17:44:37 +0200

    some fine tuning

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,7 @@ module.exports = {
       lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
       xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
       '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-     '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.25)',
       none: 'none',
     },
@@ -47,6 +47,9 @@ module.exports = {
       widest: ".05em",
     },
     extend: {
+      borderWidth: {
+       '7': '7px'
+      },  
       transitionProperty: {
         'height': 'height',
         'spacing': 'margin, padding'
