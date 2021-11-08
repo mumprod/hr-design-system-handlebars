0d77683e1b2e41a50400a62a0153bd222d65accc
Stefan Zülch <stefan.zuelch@hr.de>
2021-11-08 11:29:06 +0100

    if no theme from the theme selector dropdown is selected then display "hessenschau" theme

--- .storybook/withThemeDecorator.js
+++ .storybook/withThemeDecorator.js
@@ -1,15 +1,13 @@
 /** helper to get the active set options id */
 const getActiveValue = ({ globals, parameters }) => {
-  const global = globals.customConditionalToolbar || {};
-  const param = parameters.customConditionalToolbar;
-
-  return global[param.default]; // get the selected options id
-};
+    const global = globals.customConditionalToolbar || {}
+    const param = parameters.customConditionalToolbar
+    console.log(global[param.default])
+    return undefined !== global[param.default] ? global[param.default] : 'hessenschau' // get the selected options id
+}
 
 export const withThemeDecorator = (StoryFn, context) => {
-  return `<div class="max-w-full" data-theme="${getActiveValue(
-    context
-  )}">
+    return `<div id="theming" class="max-w-full" data-theme="${getActiveValue(context)}">
   ${StoryFn()} 
-  </div>`;
-};
+  </div>`
+}
