c18fb1fecc65c020216619d22262f3afc688c1f3
svesper <stefan.vesper@hr.de>
2022-09-01 18:01:11 +0200

    adds some of mithras designs

--- src/stories/views/components/site_header/header_alpine.js
+++ src/stories/views/components/site_header/header_alpine.js
@@ -6,6 +6,7 @@ document.addEventListener('alpine:init', () => {
     Alpine.store('burgeropen', false);
     Alpine.store('searchFieldOpen', false);
     Alpine.store('serviceNavIsOpen', false);
+    Alpine.store('contentNavDropdownIsOpen', false); 
     Alpine.store('searchID', {
         current: '{{nextRandom}}'
     });
@@ -366,15 +367,15 @@ document.addEventListener('alpine:init', () => {
 
         shouldContentBeShown(teasersize,isDropdown,isMixed){
             if (isDropdown){                   
-                return this.open                
+                return this.$store.contentNavDropdownIsOpen               
             }  
             if (isMixed) {
                 if (teasersize === 100 || teasersize === 66 || teasersize === 50) {
                     if (this.$screen('lg')) return true
                     if (this.$screen('md')) return true
-                    if (this.$screen('xs')) return this.open
+                    if (this.$screen('xs')) return this.$store.contentNavDropdownIsOpen
                 } else if (teasersize === 33 || teasersize === 25 ) { 
-                    return this.open
+                    return this.$store.contentNavDropdownIsOpen
                 }
             }
 
@@ -387,24 +388,24 @@ document.addEventListener('alpine:init', () => {
             switch (teasersize) {
                 case 100:
                     classes.push('w-full','md:w-fit')
-                    isList ? classes.push('border','mb-2', 'md:!mr-2') : isMixed ? classes.push('!w-full','md:!w-fit','!mb-0','md:!mb-2','md:mr-2','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0','md:!border') : isDropdown ? classes.push('!w-full','md:!w-full','md:mb-0','md:!mr-0','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2') : ''
+                    isList ? classes.push('border','mb-2','md:!mr-2','hover:no-underline') : isMixed ? classes.push('!w-full','md:!w-fit','!mb-0','md:!mb-2','md:mr-2','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0','md:!border','md:border-blue-congress','border-gray-400','hover:underline','hover:text-blue-congress','hover:!bg-white','hover:!fill-congress','md:hover:no-underline','md:hover:text-white','md:hover:!bg-blue-congress','md:hover:!fill-white') : isDropdown ? classes.push('!w-full','ml-0','md:!w-full','md:mb-0','md:!mr-0','border-gray-400','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0','hover:underline','hover:text-blue-congress','hover:!bg-white','hover:!fill-congress') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2','hover:no-underline') : ''
                     break;
                 case 66:
                     classes.push('w-fit')
-                    isList == true ? classes.push('border','mb-2','mr-2') : isMixed ? classes.push('!w-full','md:!w-fit','!mb-0','md:!mb-2','md:mr-2','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0','md:!border') : isDropdown ? classes.push('!w-full','md:!w-full','md:mb-0','md:!mr-0','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2') : ''
+                    isList == true ? classes.push('border','mb-2','mr-2','md:!mr-2','hover:no-underline') : isMixed ? classes.push('!w-full','md:!w-fit','!mb-0','md:!mb-2','md:mr-2','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0','md:!border','hover:text-blue-congress','hover:!fill-congress','hover:!bg-white','md:hover:!bg-blue-congress','md:hover:!text-white','md:hover:no-underline','md:hover:!fill-white') : isDropdown ? classes.push('!w-full','md:!w-full','md:mb-0','md:!mr-0','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0','hover:underline','hover:text-blue-congress','hover:!bg-white','hover:!fill-congress') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2','hover:no-underline') : ''
                     console.log(classes)
                     break;
                 case 50:
                     classes.push('w-full')
-                    isList ? classes.push('border','mb-2', 'md:!mr-2') : isMixed ? classes.push('!w-full','border','mb-2', 'md:!mr-2') : isDropdown ? classes.push('!w-full','md:!w-full','md:mb-0','md:!mr-0','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2') : ''
+                    isList ? classes.push('border','mb-2', 'md:!mr-2','hover:no-underline') : isMixed ? classes.push('!w-full','border','md:mb-2', 'md:!mr-2','last:mb-0','border-gray-400','border-l-0','border-r-0','border-t-0','md:border','md:border-blue-congress','hover:underline','hover:!bg-white','hover:text-blue-congress','hover:fill-congress','md:hover:!bg-blue-congress','md:hover:!text-white','md:hover:fill-white','md:hover:no-underline') : isDropdown ? classes.push('!w-full','md:!w-full','md:mb-0','md:!mr-0','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0','border-gray-400','hover:underline','hover:!text-blue-congress','hover:!bg-white','hover:!fill-congress') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2','hover:no-underline') : ''
                     break;
                 case 33:
                     classes.push('w-full')
-                    isList ? classes.push('border','mb-2', 'md:!mr-2') : isMixed ? classes.push('!w-full','!mb-0','border-b','last:border-b-0') : isDropdown ? classes.push('!w-full','md:!w-full','md:mb-0','md:!mr-0','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2') : ''
+                    isList ? classes.push('border','mb-2', 'md:!mr-2','hover:no-underline') : isMixed ? classes.push('!w-full','!mb-0','border-b','last:border-b-0','hover:!bg-white','hover:underline','hover:text-blue-congress','hover:!fill-congress') : isDropdown ? classes.push('!w-full','md:!w-full','md:mb-0','md:!mr-0','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0','hover:underline','hover:!text-blue-congress','hover:!bg-white','hover:!fill-congress') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2','hover:no-underline') : ''
                     break;
                 case 25:
                     classes.push('w-full')
-                    isList ? classes.push('border','mb-2', 'md:!mr-2') : isMixed ? classes.push('!w-full','!mb-0','border-b','last:border-b-0') : isDropdown ? classes.push('!w-full','md:!w-full','md:mb-0','md:!mr-0','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2') : ''
+                    isList ? classes.push('border','mb-2', 'md:!mr-2','hover:no-underline') : isMixed ? classes.push('!w-full','!mb-0','border-b','last:border-b-0','hover:!bg-white','hover:underline','hover:!text-blue-congress','hover:!fill-congress') : isDropdown ? classes.push('!w-full','md:!w-full','md:mb-0','md:!mr-0','border-l-0','border-r-0','border-t-0','border-b','last:border-b-0','hover:underline','hover:text-blue-congress','hover:!bg-white','hover:!fill-congress') : isFlow ? classes.push('!w-fit','!mr-2','border','mb-2','hover:no-underline') : ''
                     break;
 
             }
