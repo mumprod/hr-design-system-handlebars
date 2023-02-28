0c7b294eb50a460b17d549648c560518c13c164e
Mario Kinzel <mario.kinzel@hr.de>
2023-02-28 15:07:17 +0100

    FEATURE: Introduce Tracking Information for TicketButtonLink

--- src/stories/views/components/modal/modal.feature.js
+++ src/stories/views/components/modal/modal.feature.js
@@ -1,4 +1,5 @@
 import { hr$, listen, unlisten } from 'hrQuery'
+import { uxAction } from 'base/tracking/atiHelper.subfeature'
 
 const Modal = (context) => {
     const { options } = context,
@@ -6,14 +7,22 @@ const Modal = (context) => {
         modalTriggerId = options.modalTriggerId ? '#' + options.modalTriggerId : '',
         modalTrigger = hr$(modalTriggerId)[0],
         modalCloseTriggers = hr$('.js-modal-close', rootElement),
-        modal = hr$('.js-modal', rootElement)[0]
+        modal = hr$('.js-modal', rootElement)[0],
+        ticketShopButton = hr$('.js-ticket-ok-button', rootElement)[0],
+        trackingInformations = options.trackingInformations
 
     const configureEventListeners = () => {
+        listen('click', clickTracking, ticketShopButton)
         listen('click', show, modalTrigger)
 
         modalCloseTriggers.forEach((modalCloseTrigger) => {
             listen('click', close, modalCloseTrigger)
         })
+    }
+
+    const clickTracking = () => {
+        console.log("tracking")
+        uxAction(trackingInformations)
     }
 
     const show = () => {
