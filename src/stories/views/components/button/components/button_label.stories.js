10f8a5bf602ead0d15edf2c7011b0b3ec5316a6c
Stefan Zülch <stefan.zuelch@hr.de>
2023-04-25 15:45:50 +0200

    DOCUMENTATION: Adds a comment in a story to test if that triggers turbosnap to test a story

--- src/stories/views/components/button/components/button_label.stories.js
+++ src/stories/views/components/button/components/button_label.stories.js
@@ -7,6 +7,7 @@ const buttonLabelTemplate = (args) => {
     return hbsTemplate({ ...args })
 }
 
+//Kommentar zum Test von Turbosnap
 export default {
     title: 'Komponenten/Buttons/Komponenten/Label',
 
