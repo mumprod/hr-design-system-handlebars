c41ffa2ca1ecdb75682829fff480d327b1869594
svesper <stefan.vesper@hr.de>
2023-04-27 12:35:45 +0200

    fixes positioning in header and page docs

--- src/stories/views/components/page/page.data.js
+++ src/stories/views/components/page/page.data.js
@@ -2,8 +2,10 @@ import structuredClone from 'core-js-pure/actual/structured-clone'
 import NavigationData from '../site_header/fixtures/site_header_default.json'
 import NavigationData2 from '../site_header/fixtures/site_header_mit_warnung.json'
 import NavigationData3 from '../site_header/fixtures/site_header_mit_submenu.json'
+import NavigationData4 from '../site_header/fixtures/site_header_mit_top_topics.json'
 
 import hero_teaser from '../teaser/fixtures/teaser_standard_hero_serif.json'
+
 const NavigationDataWithTeaser = structuredClone(
     Object.assign({}, NavigationData, hero_teaser.logicItem.includeModel)
 )
@@ -13,5 +15,7 @@ const NavigationDataWithTeaser2 = structuredClone(
 const NavigationDataWithTeaser3 = structuredClone(
     Object.assign({}, NavigationData3, hero_teaser.logicItem.includeModel)
 )
-
-export { NavigationDataWithTeaser, NavigationDataWithTeaser2, NavigationDataWithTeaser3 }
+const NavigationDataWithTeaser4 = structuredClone(
+    Object.assign({}, NavigationData4, hero_teaser.logicItem.includeModel)
+)
+export { NavigationDataWithTeaser, NavigationDataWithTeaser2, NavigationDataWithTeaser3, NavigationDataWithTeaser4 }
