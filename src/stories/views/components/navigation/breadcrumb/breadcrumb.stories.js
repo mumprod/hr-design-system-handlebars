d646b9d0253fcad0dbf435a5263c33dc09d12f82
Stefan Zülch <stefan.zuelch@hr.de>
2023-05-10 16:27:42 +0200

    FEATURE: Breadcrumb is implented in its basic version and tested with stories from 2 up to 5 breadrumb items

--- src/stories/views/components/navigation/breadcrumb/breadcrumb.stories.js
+++ src/stories/views/components/navigation/breadcrumb/breadcrumb.stories.js
@@ -1,25 +1,26 @@
+import breadcrumb from './breadcrumb.hbs'
+import breadcrumb2LevelData from './fixtures/breadcrumb_2_level.json'
+import breadcrumb3LevelData from './fixtures/breadcrumb_3_level.json'
+import breadcrumb4LevelData from './fixtures/breadcrumb_4_level.json'
+import breadcrumb5LevelData from './fixtures/breadcrumb_5_level.json'
+
+const Template = (args) => {
+    // You can either use a function to create DOM elements or use a plain html string!
+    // return `<div>${label}</div>`;
+    return breadcrumb({ ...args })
+}
 export default {
     title: 'Komponenten/Navigation/Breadcrumb',
 
     argTypes: {
-        _label: {
-            description: 'Label des Buttons (wird ausschließlich Screenreader Nutzern vorgelesen)',
+        _currentPageUrl: {
+            description: 'URL der aktuellen Seite',
             control: 'text',
         },
 
-        _icon: {
-            control: {
-                type: 'select',
-                options: ['play_button', 'audio_button', 'podcast-button'],
-            },
-
-            description: 'Das Icon des Buttons',
-        },
-
-        _isLivestream: {
-            description:
-                'Legt die Farbe des Buttons fest. Ist es ein Livestream, hat er eine andere Farbe.',
-            control: 'boolean',
+        _currentPageTitle: {
+            control: 'text',
+            description: 'Titel der aktuellen Seite',
         },
     },
 
@@ -27,5 +28,55 @@ export default {
         controls: {
             sort: 'alpha',
         },
+    },
+    decorators: [
+        (Story) => {
+            return `<div class="grid grid-page"><div class="grid-cols-12 py-6 col-full sm:px-9.5 sm:col-main">  
+             ${Story()} 
+             </div></div>`
+        },
+    ],
+}
+
+export const breadcrumb2Level = {
+    render: Template.bind({}),
+    name: 'Breadcrumb 2 Level',
+    args: {
+        _currentPageUrl: '#currentPage',
+        _currentPageTitle: 'Kurzer Titel',
+        ...breadcrumb2LevelData.breadcrumb,
+    },
+}
+
+export const breadcrumb3Level = {
+    render: Template.bind({}),
+    name: 'Breadcrumb 3 Level',
+    args: {
+        _currentPageUrl: '#currentPage',
+        _currentPageTitle:
+            'Korruptionsprozess: Verteidigung von Ex-Oberstaatsanwalt plädiert auf höchstens vier Jahre Haft',
+        ...breadcrumb3LevelData.breadcrumb,
+    },
+}
+
+export const breadcrumb4Level = {
+    render: Template.bind({}),
+    name: 'Breadcrumb 4 Level',
+    args: {
+        _currentPageUrl: '#currentPage',
+        _currentPageTitle:
+            'Darmstadt 98 bleibt trotz Enttäuschung cool: "Keinen Stift in der Hose" ',
+        ...breadcrumb4LevelData.breadcrumb,
+    },
+}
+
+export const breadcrumb5Level = {
+    render: Template.bind({}),
+    name: 'Breadcrumb 5 Level',
+    args: {
+        _currentPageUrl: '#currentPage',
+        _currentPageTitle:
+            'Verein bestätigt: Eintracht Frankfurt trennt sich im Sommer von Oliver Glasner',
+        ...breadcrumb5LevelData.breadcrumb,
     },
 }
