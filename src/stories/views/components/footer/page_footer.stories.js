208d512a98ca91888102548b1fa1d3da1a60412c
MPiller <Michael.Piller@hr.de>
2023-06-19 22:06:55 +0200

    - Adding data for hr wellen - Adding docu

--- src/stories/views/components/footer/page_footer.stories.js
+++ src/stories/views/components/footer/page_footer.stories.js
@@ -1,4 +1,13 @@
-import { FooterContent } from './page_footer.data.js'
+import { 
+    FooterContentHessenschau,
+    FooterContentHR1,
+    FooterContentHR2,
+    FooterContentHR3,
+    FooterContentHR4,
+    FooterContentHRINFO,
+    FooterContentYOUFM,
+
+} from './page_footer.data.js'
 import pageFooter from './page_footer.hbs'
 
 const TemplatePageFooter = (args, { globals: { customConditionalToolbar } }) => {
@@ -23,8 +32,39 @@ export default {
     },
 }
 
-export const Seitenfooter = {
+export const SeitenfooterHessenschau = {
     render: TemplatePageFooter.bind({}),
-    name: 'Seitenfooter',
-    args: FooterContent,
+    name: 'Seitenfooter hessenschau',
+    args: FooterContentHessenschau,
+}
+
+export const SeitenfooterHR1 = {
+    render: TemplatePageFooter.bind({}),
+    name: 'Seitenfooter hr1',
+    args: FooterContentHR1,
+}
+export const SeitenfooterHR2 = {
+    render: TemplatePageFooter.bind({}),
+    name: 'Seitenfooter hr2',
+    args: FooterContentHR2,
+}
+export const SeitenfooterHR3 = {
+    render: TemplatePageFooter.bind({}),
+    name: 'Seitenfooter hr3',
+    args: FooterContentHR3,
+}
+export const SeitenfooterHR4 = {
+    render: TemplatePageFooter.bind({}),
+    name: 'Seitenfooter hr4',
+    args: FooterContentHR4,
+}
+export const SeitenfooterHRINFO = {
+    render: TemplatePageFooter.bind({}),
+    name: 'Seitenfooter hriNFO',
+    args: FooterContentHRINFO,
+}
+export const SeitenfooterYOUFM = {
+    render: TemplatePageFooter.bind({}),
+    name: 'Seitenfooter You FM',
+    args: FooterContentYOUFM,
 }
