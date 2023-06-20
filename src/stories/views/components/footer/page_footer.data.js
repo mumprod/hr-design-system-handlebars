3c47178c02d3ababfd60f0b7a4b241ff8e9ce912
svesper <stefan.vesper@hr.de>
2023-06-20 12:25:36 +0200

    refac jsons and hbs

--- src/stories/views/components/footer/page_footer.data.js
+++ src/stories/views/components/footer/page_footer.data.js
@@ -1,11 +1,11 @@
 import structuredClone from 'core-js-pure/actual/structured-clone'
-import footerContentHessenschau from '../page/fixtures/footer_hessenschau.json'
-import footerContentHR1 from '../page/fixtures/footer_hr1.json'
-import footerContentHR2 from '../page/fixtures/footer_hr2.json'
-import footerContentHR3 from '../page/fixtures/footer_hr3.json'
-import footerContentHR4 from '../page/fixtures/footer_hr4.json'
-import footerContentHRINFO from '../page/fixtures/footer_hrinfo.json'
-import footerContentYOUFM from '../page/fixtures/footer_youfm.json'
+import footerContentHessenschau from './fixtures/footer_hessenschau.json'
+import footerContentHR1 from './fixtures/footer_hr1.json'
+import footerContentHR2 from './fixtures/footer_hr2.json'
+import footerContentHR3 from './fixtures/footer_hr3.json'
+import footerContentHR4 from './fixtures/footer_hr4.json'
+import footerContentHRINFO from './fixtures/footer_hrinfo.json'
+import footerContentYOUFM from './fixtures/footer_youfm.json'
 const FooterContentHessenschau =  structuredClone(
     Object.assign({}, footerContentHessenschau)
 )
