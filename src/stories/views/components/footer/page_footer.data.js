0d50082b9d894fc2b8a32031fde9b9a940b7625c
svesper <stefan.vesper@hr.de>
2023-07-07 13:04:04 +0200

    new icon, less stories

--- src/stories/views/components/footer/page_footer.data.js
+++ src/stories/views/components/footer/page_footer.data.js
@@ -1,37 +1,9 @@
 import structuredClone from 'core-js-pure/actual/structured-clone'
-import footerContentHessenschau from './fixtures/footer_hessenschau.json'
-import footerContentHR1 from './fixtures/footer_hr1.json'
-import footerContentHR2 from './fixtures/footer_hr2.json'
-import footerContentHR3 from './fixtures/footer_hr3.json'
-import footerContentHR4 from './fixtures/footer_hr4.json'
-import footerContentHRINFO from './fixtures/footer_hrinfo.json'
-import footerContentYOUFM from './fixtures/footer_youfm.json'
-const FooterContentHessenschau =  structuredClone(
-    Object.assign({}, footerContentHessenschau)
+import footerContent from './fixtures/footer_hessenschau.json'
+
+const FooterContent =  structuredClone(
+    Object.assign({}, footerContent)
 )
-const FooterContentHR1 =  structuredClone(
-    Object.assign({}, footerContentHR1)
-)
-const FooterContentHR2 =  structuredClone(
-    Object.assign({}, footerContentHR2)
-)
-const FooterContentHR3 =  structuredClone(
-    Object.assign({}, footerContentHR3)
-)
-const FooterContentHR4 =  structuredClone(
-    Object.assign({}, footerContentHR4)
-)
-const FooterContentHRINFO =  structuredClone(
-    Object.assign({}, footerContentHRINFO)
-)
-const FooterContentYOUFM =  structuredClone(
-    Object.assign({}, footerContentYOUFM)
-)
-export { FooterContentHessenschau }
-export { FooterContentHR1 }
-export { FooterContentHR2 }
-export { FooterContentHR3 }
-export { FooterContentHR4 }
-export { FooterContentHRINFO }
-export { FooterContentYOUFM }
+
+export { FooterContent }
 
--- src/stories/views/components/footer/page_footer.stories.js
+++ src/stories/views/components/footer/page_footer.stories.js
@@ -1,23 +1,12 @@
 import { 
-    FooterContentHessenschau,
-    FooterContentHR1,
-    FooterContentHR2,
-    FooterContentHR3,
-    FooterContentHR4,
-    FooterContentHRINFO,
-    FooterContentYOUFM,
-
+    FooterContent
 } from './page_footer.data.js'
 import pageFooter from './page_footer.hbs'
 
 const TemplatePageFooter = (args, { globals: { theme } }) => {
     // You can either use a function to create DOM elements or use a plain html string!
     // return `<div>${label}</div>`;
-    let brand = undefined !== theme ? theme : 'hessenschau'
-    console.log(args)
-
-
-        
+    let brand = undefined !== theme ? theme : 'hessenschau'       
     return pageFooter({ brand, ...args })
 }
 
@@ -34,39 +23,8 @@ export default {
     },
 }
 
-export const SeitenfooterHessenschau = {
+export const Seitenfooter = {
     render: TemplatePageFooter.bind({}),
-    name: 'Seitenfooter hessenschau',
-    args: FooterContentHessenschau, 
-}
-
-export const SeitenfooterHR1 = {
-    render: TemplatePageFooter.bind({}),
-    name: 'Seitenfooter hr1',
-    args: FooterContentHR1
-}
-export const SeitenfooterHR2 = {
-    render: TemplatePageFooter.bind({}),
-    name: 'Seitenfooter hr2',
-    args: FooterContentHR2,
-}
-export const SeitenfooterHR3 = {
-    render: TemplatePageFooter.bind({}),
-    name: 'Seitenfooter hr3',
-    args: FooterContentHR3,
-}
-export const SeitenfooterHR4 = {
-    render: TemplatePageFooter.bind({}),
-    name: 'Seitenfooter hr4',
-    args: FooterContentHR4,
-}
-export const SeitenfooterHRINFO = {
-    render: TemplatePageFooter.bind({}),
-    name: 'Seitenfooter hriNFO',
-    args: FooterContentHRINFO,
-}
-export const SeitenfooterYOUFM = {
-    render: TemplatePageFooter.bind({}),
-    name: 'Seitenfooter You FM',
-    args: FooterContentYOUFM,
+    name: 'Seitenfooter',
+    args: FooterContent, 
 }
