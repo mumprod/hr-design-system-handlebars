6e93c7b0ba677e0be954814c38cadb81d9fa9a32
Stefan Zülch <stefan.zuelch@hr.de>
2023-08-07 10:17:13 +0200

    Changes the title of the table of contents to Inhalt

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -158,7 +158,7 @@ export const parameters = {
             contentsSelector: '.sbdocs-content',
             headingSelector: 'h1, h2, h3',
             ignoreSelector: '#primary',
-            title: 'Table of Contents',
+            title: 'Inhalt',
             disable: false,
             unsafeTocbotOptions: {
                 orderedList: false,
