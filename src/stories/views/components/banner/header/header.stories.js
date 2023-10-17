ee018a7a0ef038f146f541230bcda36511b9007f
vascoeduardo <v.m.p.eduardo@googlemail.com>
2023-10-17 15:24:16 +0200

    - new templates and story for banner headlines

---
+++ src/stories/views/components/banner/header/header.stories.js
@@ -0,0 +1,27 @@
+import header from './header.hbs'
+
+const TemplateHeader = (args, { globals: { theme } }) => {
+    // You can either use a function to create DOM elements or use a plain html string!
+    // return `<div>${label}</div>`;
+    let brand = undefined !== theme ? theme : 'hessenschau'       
+    return header({ brand, ...args })
+}
+
+export default {
+    title: 'Komponenten/Banner/Header',
+    argTypes: {},
+
+    parameters: {
+        layout: 'fullscreen',
+        docs: {
+            inlineStories: false,
+            iframeHeight: 400,
+        },
+    },
+}
+
+export const Seitenfooter = {
+    render: TemplateHeader.bind({}),
+    name: 'BannerHeader',
+  
+}
