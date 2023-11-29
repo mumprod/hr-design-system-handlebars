9395dad421eb7229da7c4cc549f8f90726625e76
Gerald Ohl <gerald.ohl@hr.de>
2023-11-29 17:07:05 +0100

    include poster-teaser in copytext

--- src/stories/views/components/content/copytext/copytext.stories.js
+++ src/stories/views/components/content/copytext/copytext.stories.js
@@ -1,5 +1,6 @@
 import copytext from './copytext.hbs'
 import copytext_json from './fixtures/copytext.json'
+import copytext_posterteaser_json from './fixtures/copytext_posterteaser.json'
 
 const Template = ({ ...args }) => {
     return copytext({ ...args })
@@ -13,4 +14,10 @@ export const Default = {
     render: Template.bind({}),
     name: 'default',
     args: copytext_json,
+}
+
+export const WithPosterteaser = {
+    render: Template.bind({}),
+    name: 'Posterteaser',
+    args: copytext_posterteaser_json,
 }
