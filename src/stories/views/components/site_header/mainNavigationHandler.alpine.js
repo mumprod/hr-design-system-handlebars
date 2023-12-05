3e2a0f3406932bde2521ab7006333f1af1853e1c
svesper <stefan.vesper@hr.de>
2023-12-05 13:10:49 +0100

    testing 1

--- src/stories/views/components/site_header/mainNavigationHandler.alpine.js
+++ src/stories/views/components/site_header/mainNavigationHandler.alpine.js
@@ -41,9 +41,9 @@ export default () => ({
             //console.log('Scroll initiated by ' + (window.userScroll == true ? "user" : "browser"));
         }
         // Listeners
-        window.addEventListener('mousedown', mouseDownHandler, false)
-        window.addEventListener('wheel', mouseEvent, false)
-        window.addEventListener('touchmove', mouseEvent, false)
+        window.addEventListener('mousedown', mouseDownHandler,false)
+        window.addEventListener('wheel', mouseEvent, {passive: true})
+        window.addEventListener('touchmove', mouseEvent, {passive: true})
         window.addEventListener('scroll', this.debounce(scrollHandler, 50), { passive: true })
     },
     //Holds the percentage of scrolled viewport
