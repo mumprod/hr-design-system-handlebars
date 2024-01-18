7924d24c73c006a11b20941eb024a6bb2c092acd
Gerald Ohl <gerald.ohl@hr.de>
2024-01-18 15:21:22 +0100

    add copytext-podcast component

--- src/stories/views/components/content/copytext/copytext.stories.js
+++ src/stories/views/components/content/copytext/copytext.stories.js
@@ -3,6 +3,7 @@ import copytext_json from './fixtures/copytext.json'
 import copytext_posterteaser_json from './fixtures/copytext_posterteaser.json'
 import copytext_additionalInfo_json from './fixtures/copytext_additionalInfo.json'
 import copytext_cite_json from './fixtures/copytext_cite.json'
+import copytext_podcastepisode_json from './fixtures/copytext_podcastepisode.json'
 
 const Template = ({ ...args }) => {
     return copytext({ ...args })
@@ -16,6 +17,12 @@ export const Default = {
     render: Template.bind({}),
     name: 'default',
     args: copytext_json,
+}
+
+export const WithPodcastEpisode = {
+    render: Template.bind({}),
+    name: 'Podcastepisode',
+    args: copytext_podcastepisode_json,
 }
 
 export const WithPosterteaser = {
