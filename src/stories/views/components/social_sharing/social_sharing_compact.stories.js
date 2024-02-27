ec3eed2f5b4a79cca2dc96b3d2a2ba972f2adc9a
vascoeduardo <v.m.p.eduardo@googlemail.com>
2024-02-27 17:30:26 +0100

    :sparkles: DPE-2816 Sticky sharing - init - different style for sticky/non-sticky - nativ sharing init

---
+++ src/stories/views/components/social_sharing/social_sharing_compact.stories.js
@@ -0,0 +1,25 @@
+import story_template from './social_sharing_compact.hbs'
+
+const Template = ({ ...args }) => {
+    // You can either use a function to create DOM elements or use a plain html string!
+    // return `<span>${topline}</span>`;
+    return story_template({ ...args })
+}
+
+export default {
+    title: 'Komponenten/Social Sharing',
+}
+
+export const Default = {
+    render: Template.bind({}),
+    name: 'Social Sharing Kompakt',
+    parameters: {
+    },
+    decorators: [
+        (Story) => {
+            return `<div class="w-full h-[500rem]">  
+             ${Story()} 
+             </div>`
+        },
+    ],
+}
