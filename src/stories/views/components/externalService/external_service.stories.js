6faea1cabe238f1c4ac7cdc906ae7b4949943a92
mpiller <Michael.Piller@hr.de>
2024-03-11 15:26:25 +0100

    Adding new design elements

--- src/stories/views/components/externalService/external_service.stories.js
+++ src/stories/views/components/externalService/external_service.stories.js
@@ -11,7 +11,7 @@ const TemplatePageExternalService = (args, { globals: { theme } }) => {
 }
 
 export default {
-    title: 'Komponenten/Externe_Dienste',
+    title: 'Komponenten/Externe Dienste',
     argTypes: {},
 
     parameters: {
