16c436dd704b5e3a1aa287eba768732e8f463c23
mpiller <Michael.Piller@hr.de>
2024-04-11 12:20:14 +0200

    Fixing style

--- src/stories/views/components/externalService/externalServiceDs.feature.js
+++ src/stories/views/components/externalService/externalServiceDs.feature.js
@@ -231,7 +231,7 @@ const ExternalService = function (context) {
             embedCode +
             "?utm_source=ig_embed&amp;utm_campaign=loading'" +
             "data-instgrm-version='14'" +
-            "style='background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:660px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);' >" +
+            "style='background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:724px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);' >" +
             '</blockquote>'
         replaceAnimated(rootElement, instagramEmbedCode, false, reloadInstagramEmbed)
     }
@@ -360,9 +360,9 @@ const ExternalService = function (context) {
     }
     const toggleContentSettingsButton = function () {
         if (isExternalServiceLoaded) {
-            contentSettingsButton.classList.remove('hideExtButton')
+            contentSettingsButton.classList.remove('hidden')
         } else {
-            contentSettingsButton.classList.add('hideExtButton')
+            contentSettingsButton.classList.add('hidden')
         }
         console.log('Toggle den Einstellungsbutton außerhalb weil ' + isExternalServiceLoaded)
     }
