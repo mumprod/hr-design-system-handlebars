afde89eb2bdad3a5fd50c519d3393bcc354b0f98
svesper <stefan.vesper@hr.de>
2024-04-25 12:39:36 +0200

    removes flickering when switch to mobile in browser

--- src/assets/js/alpine.js
+++ src/assets/js/alpine.js
@@ -41,9 +41,7 @@ Alpine.store('subNavIsVisible', false)
 Alpine.store('footerIsVisible', false)
 Alpine.store('sectionNavIsVisible', false)
 Alpine.store('sharingIsVisible', true)
-Alpine.store('globalPercent', {
-    current: '0'
-})
+Alpine.store('sharingIsOpen', false)
 Alpine.store('sharingBottomPos', {
     current: '0'
 })
