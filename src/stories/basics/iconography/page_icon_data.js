77895e8b44134705af3b59f5629d3184b8b72684
mpiller <Michael.Piller@hr.de>
2024-07-01 16:05:57 +0200

    Dynamic Icon Generation

--- src/stories/basics/iconography/page_icon_data.js
+++ src/stories/basics/iconography/page_icon_data.js
@@ -3,12 +3,12 @@ const images = require.context('./images', true);
 let imageList = {}
 let fileName
 let svgString
-let svgList = []
+let svgList = {}
 imageList = images.keys().map(image => images(image));
+
 for(let i=0; i < imageList.length; i++){
     fileName = imageList[i].replace(/^.*[\\/]/, '').slice(0,-4)
-    svgString = "<svg class='svg-item' viewBox='0 0 100 100' width='100px' height='100px'><use xlink:href='./icons/icons/svgmap.min.svg#"+fileName+"'></use></svg>"
-    svgList.push(svgString)
+    svgList[fileName] = "./icons/icons/svgmap.min.svg#"+fileName
 }
-console.log(svgList)
+
 export default svgList;
