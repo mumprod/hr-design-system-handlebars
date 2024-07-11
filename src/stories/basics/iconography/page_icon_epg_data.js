28667af783ff185471db26a36ab7b67b8f2a2a3e
mpiller <Michael.Piller@hr.de>
2024-07-11 14:19:23 +0200

    Changing Object names

--- src/stories/basics/iconography/page_icon_epg_data.js
+++ src/stories/basics/iconography/page_icon_epg_data.js
@@ -2,12 +2,12 @@ const images = require.context('/src/assets/icons/epg/svgmap', true);
 
 let imageList = {}
 let file
-let svgList = {}
+let svgListEPG = {}
 imageList = images.keys().map(image => images(image));
 
 for(let i=0; i < imageList.length; i++){
     file = imageList[i].replace(/^.*[\\/]/, '').slice(0,-4)
-    svgList[file] = file
+    svgListEPG[file] = file
 }
 
-export default svgList;
+export default svgListEPG;
--- src/stories/basics/iconography/page_icon_traffic_data.js
+++ src/stories/basics/iconography/page_icon_traffic_data.js
@@ -2,12 +2,12 @@ const images = require.context('/src/assets/icons/traffic/svgmap', true);
 
 let imageList = {}
 let file
-let svgList = {}
+let svgListTraffic = {}
 imageList = images.keys().map(image => images(image));
 
 for(let i=0; i < imageList.length; i++){
     file = imageList[i].replace(/^.*[\\/]/, '').slice(0,-4)
-    svgList[file] = file
+    svgListTraffic[file] = file
 }
 
-export default svgList;
+export default svgListTraffic;
