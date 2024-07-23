0aaa14d793424729c291402d488e3dda880c3e66
vascoeduardo <v.m.p.eduardo@googlemail.com>
2024-07-23 13:09:38 +0200

    sharing is handled completly in script

--- src/stories/views/components/social_sharing/socialSharingHandler.alpine.js
+++ src/stories/views/components/social_sharing/socialSharingHandler.alpine.js
@@ -25,15 +25,19 @@ export default (module) => ({
             .catch((error) => console.error('Sharing failed:', error));
         } else {
             if (this.$store.sharingIsOpen[module]) {
-                return this.close()
+                return this.close(true)
             }
             this.$refs.button.focus()
             this.$store.sharingIsOpen[module] = true
+            uxAction('socialShareClick::'+module+'::sharingIconOpen');
         }               
     },
-    close() {
+    close(trackClick) {
         if (!this.$store.sharingIsOpen[module]) return
         this.$store.sharingIsOpen[module] = false
+        if(trackClick) {
+            uxAction('socialShareClick::'+module+'::sharingIconClose');
+        }
     }
     
 })
