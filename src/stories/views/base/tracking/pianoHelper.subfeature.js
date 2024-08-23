1f4f6b9ba6e76075882e0ccb18f4ecadb95acbc0
Stefan Zülch <stefan.zuelch@hr.de>
2024-08-23 14:50:25 +0200

    :recycle: Optimize the javascript code for a smaller filesize

--- src/stories/views/base/tracking/pianoHelper.subfeature.js
+++ src/stories/views/base/tracking/pianoHelper.subfeature.js
@@ -1,44 +1,36 @@
-import SettingsCookie from 'components/externalService/globalSettingsCookie.subfeature'
+import SettingsCookie from 'components/externalService/globalSettingsCookie.subfeature';
 
 const isTrackingAllowed = () => {
-    const settingsCookie = new SettingsCookie()
-    return settingsCookie.isSettingsCookieAccepted('ati')
-}
+    const settingsCookie = new SettingsCookie();
+    return settingsCookie.isSettingsCookieAccepted('ati');
+};
 
-const uxAction = (label, secondLevelId) => {
-    secondLevelId = undefined !== secondLevelId ? secondLevelId : typeof pageDisplayConfig !== "undefined" && pageDisplayConfig !== undefined ? pageDisplayConfig.site_level2_id:99
-    if (typeof pa !== "undefined" && pa !== undefined && isTrackingAllowed()) {
-        pa.sendEvent('click.action', {
-            click: label,
-            site_level2_id: secondLevelId,
-        })
+const sendEvent = (eventType, event) => {
+    if (typeof pa !== 'undefined' && pa !== undefined && isTrackingAllowed()) {
+        pa.sendEvent(eventType, event);
     }
-}
+};
 
-const uxNavigation = (label, secondLevelId) => {
-    secondLevelId = undefined !== secondLevelId ? secondLevelId : typeof pageDisplayConfig !== "undefined" && pageDisplayConfig !== undefined ? pageDisplayConfig.site_level2_id:99
-    if (typeof pa !== "undefined" && pa !== undefined  && isTrackingAllowed()) {
-        pa.sendEvent('click.navigation', {
-            click: label,
-            site_level2_id: secondLevelId,
-        })
+const getDefaultEvent = (label, secondLevelId) => {
+    return {
+        click: label,
+        site_level2_id: determineSecondLevelId(secondLevelId)
     }
-}
+};
 
-const pi = () => {
-    if (typeof pa !== "undefined" && pa !== undefined  && isTrackingAllowed()) {
-        pa.sendEvent('page.display', pageDisplayConfig)
-    }
-}
+const determineSecondLevelId = (secondLevelId) => {
+    return secondLevelId !== undefined
+        ? secondLevelId
+        : pageDisplayConfig.site_level2_id;
+};
 
-const download = (label, secondLevelId) => {
-    secondLevelId = undefined !== secondLevelId ? secondLevelId : typeof pageDisplayConfig !== "undefined" && pageDisplayConfig !== undefined ? pageDisplayConfig.site_level2_id:99
-    if (typeof pa !== "undefined" && pa !== undefined  && isTrackingAllowed()) {
-        pa.sendEvent('click.download', {
-            click: label,
-            site_level2_id: secondLevelId,
-        })
-    }
-}
+const uxAction = (label, secondLevelId) => sendEvent('click.action', getDefaultEvent(label, secondLevelId));
 
-export { isTrackingAllowed, uxAction, uxNavigation, pi, download }
+const uxNavigation = (label, secondLevelId) => sendEvent('click.navigation', getDefaultEvent(label, secondLevelId));
+
+const pi = () => sendEvent('page.display', pageDisplayConfig);
+
+const download = (label, secondLevelId) => sendEvent('click.download', getDefaultEvent(label, secondLevelId));
+
+
+export { isTrackingAllowed, uxAction, uxNavigation, pi, download };
