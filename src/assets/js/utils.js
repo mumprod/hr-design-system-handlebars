5de8f3c52401bde3809cff2f183fab0c3b79cca2
Stefan Zülch <stefan.zuelch@hr.de>
2024-08-30 10:39:42 +0200

    :recycle: Change method name

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,14 +1,18 @@
-const snapshotsTemplate = (args) => {
-    const { hbsTemplates, snapshotsJson } = args
-
+const getSnapshotsTemplate = (args) => {
+    const { hbsTemplates, snapshotsJson, noContainer } = args
     let template = '<div class="flex flex-auto flex-wrap gap-3">'
     for (const [key, value] of Object.entries(snapshotsJson)) {
-        template += '<div>'
+        if (!noContainer) {
+            template += '<div>'
+        }
+        console.log("Hubsi", hbsTemplates[value.template])
         template += hbsTemplates[value.template]({ ...value.args })
-        template += '</div>'
+        if (!noContainer) {
+            template += '</div>'
+        }
     }
     template += '</div>'
     return template
 }
 
-export { snapshotsTemplate }
+export { getSnapshotsTemplate }
