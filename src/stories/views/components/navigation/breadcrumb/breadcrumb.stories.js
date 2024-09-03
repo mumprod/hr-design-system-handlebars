cac493c823b47ee9743f14e0667be54964da15ed
Stefan Zülch <stefan.zuelch@hr.de>
2024-09-03 09:09:28 +0200

    :sparkles: Add snapshot story for breadcrumb component. Refactor the breadcrumb json

--- src/stories/views/components/navigation/breadcrumb/breadcrumb.stories.js
+++ src/stories/views/components/navigation/breadcrumb/breadcrumb.stories.js
@@ -1,13 +1,21 @@
-import breadcrumb from './breadcrumb.hbs'
-import breadcrumb2LevelData from './fixtures/breadcrumb_2_level.json'
-import breadcrumb3LevelData from './fixtures/breadcrumb_3_level.json'
-import breadcrumb4LevelData from './fixtures/breadcrumb_4_level.json'
-import breadcrumb5LevelData from './fixtures/breadcrumb_5_level.json'
+import { getSnapshotsTemplate } from '/src/assets/js/utils.js'
+import snapshotsJson from './fixtures/breadcrumb.json'
+
+const handlebars = require('hrHandlebars')
+const hbsTemplates = []
+hbsTemplates['default'] = handlebars.compile(`
+    {{> components/navigation/breadcrumb/breadcrumb}}  
+  `)
 
 const Template = (args) => {
     // You can either use a function to create DOM elements or use a plain html string!
     // return `<div>${label}</div>`;
-    return breadcrumb({ ...args })
+    console.log("Args: ", { ...args })
+    return hbsTemplates['default']({ ...args })
+}
+
+const snapshotTemplate = (args) => {
+    return getSnapshotsTemplate({ hbsTemplates, ...args })
 }
 
 export default {
@@ -44,6 +52,9 @@ export default {
         controls: {
             sort: 'alpha',
         },
+        chromatic: {
+            disableSnapshot: true
+        },
     },
     decorators: [
         (Story) => {
@@ -60,42 +71,33 @@ export default {
 export const breadcrumb2Level = {
     render: Template.bind({}),
     name: 'Breadcrumb 2 Level',
-    args: {
-        _currentPageUrl: '#currentPage',
-        _currentPageTitle: 'Kurzer Titel',
-        ...breadcrumb2LevelData.breadcrumb,
-    },
+    args: snapshotsJson['2_levels'].args,
 }
 
 export const breadcrumb3Level = {
     render: Template.bind({}),
     name: 'Breadcrumb 3 Level',
-    args: {
-        _currentPageUrl: '#currentPage',
-        _currentPageTitle:
-            'Korruptionsprozess: Verteidigung von Ex-Oberstaatsanwalt plädiert auf höchstens vier Jahre Haft',
-        ...breadcrumb3LevelData.breadcrumb,
-    },
+    args: snapshotsJson['3_levels'].args,
 }
 
 export const breadcrumb4Level = {
     render: Template.bind({}),
     name: 'Breadcrumb 4 Level',
-    args: {
-        _currentPageUrl: '#currentPage',
-        _currentPageTitle:
-            'Darmstadt 98 bleibt trotz Enttäuschung cool: "Keinen Stift in der Hose" ',
-        ...breadcrumb4LevelData.breadcrumb,
-    },
+    args: snapshotsJson['4_levels'].args,
 }
 
 export const breadcrumb5Level = {
     render: Template.bind({}),
     name: 'Breadcrumb 5 Level',
-    args: {
-        _currentPageUrl: '#currentPage',
-        _currentPageTitle:
-            'Verein bestätigt: Eintracht Frankfurt trennt sich im Sommer von Oliver Glasner',
-        ...breadcrumb5LevelData.breadcrumb,
-    },
+    args: snapshotsJson['5_levels'].args,
+}
+
+export const Snapshot = {
+    render: snapshotTemplate.bind({}),
+    name: 'Snapshot',
+
+    args: { snapshotsJson },
+    parameters: {
+        chromatic: { disableSnapshot: false },
+    }
 }
