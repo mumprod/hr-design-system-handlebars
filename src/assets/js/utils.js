35031d54659895ca097d43ae56bcabc414f2aa19
Stefan Zülch <stefan.zuelch@hr.de>
2024-09-09 09:54:39 +0200

    :recycle: Optimize the code

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,32 +1,30 @@
 const getSnapshotsTemplate = (args) => {
-    const { hbsTemplates, args: snapshotsJson } = args,
-        config = snapshotsJson.config || {},
-        renderSnapshotsWrapper = undefined !== config.renderSnapshotsWrapper ? config.renderSnapshotsWrapper : true,
-        filteredSnapshots = Object.entries(snapshotsJson).filter(([key, value]) => {
-            if (key === "config") {
-                return false;
-            }
+    const { hbsTemplates, args: snapshotsJson } = args
+    const config = snapshotsJson.config || {}
+    const renderSnapshotsWrapper = undefined !== config.renderSnapshotsWrapper ? config.renderSnapshotsWrapper : true
 
-            if (value.config === undefined) {
-                return true;
-            }
-
-            return value.config.useAsSnapshot !== false;
-        })
+    const filteredSnapshots = Object.entries(snapshotsJson).filter(([key, value]) =>
+        key !== "config" && (value.config === undefined || value.config.useAsSnapshot !== false)
+    )
     let template = renderSnapshotsWrapper ? '<div class="flex flex-auto flex-wrap gap-8">' : ''
-    for (const [key, value] of filteredSnapshots) {
-        const snapshotItemConfig = value.config || {},
-            renderSnapshotItemWrapper = undefined !== snapshotItemConfig.renderSnapshotItemWrapper ? snapshotItemConfig.renderSnapshotItemWrapper : true,
-            path = config.path ? `args.${config.path}` : snapshotItemConfig.path ? `args.${snapshotItemConfig.path}` : "args",
-            snapshotTemplateName = undefined !== snapshotItemConfig.template ? snapshotItemConfig.template : undefined !== config.template ? config.template : "no-template"
+
+    filteredSnapshots.forEach(([key, value]) => {
+        const snapshotItemConfig = value.config || {}
+        const renderSnapshotItemWrapper = undefined !== snapshotItemConfig.renderSnapshotItemWrapper ? snapshotItemConfig.renderSnapshotItemWrapper : true
+        const path = config.path ? `args.${config.path}` : (snapshotItemConfig.path ? `args.${snapshotItemConfig.path}` : "args")
+        const snapshotTemplateName = snapshotItemConfig.template || config.template || "no-template"
+
         if (renderSnapshotsWrapper && renderSnapshotItemWrapper) {
-            template += snapshotItemConfig.css === undefined ? '<div>' : `<div class="${snapshotItemConfig.css}">`
+            template += `<div${snapshotItemConfig.css ? ` class="${snapshotItemConfig.css}"` : ''}>`
         }
-        template += hbsTemplates[snapshotTemplateName](resolveObjectFromPath(path, value))
+
+        template += hbsTemplates[snapshotTemplateName](resolveObjectFromPath(path, value));
+
         if (renderSnapshotsWrapper && renderSnapshotItemWrapper) {
-            template += '</div>'
+            template += '</div>';
         }
-    }
+    })
+
     if (renderSnapshotsWrapper) {
         template += '</div>'
     }
