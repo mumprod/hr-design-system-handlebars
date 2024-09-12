fcbe429c43132585d097e4289e563bf004c81828
Stefan Zülch <stefan.zuelch@hr.de>
2024-09-12 14:15:35 +0200

    :sparkles: Add snapshot story for poster teaser component

--- src/stories/views/components/teaser/teaser_poster.stories.js
+++ src/stories/views/components/teaser/teaser_poster.stories.js
@@ -1,15 +1,25 @@
+import { getSnapshotsTemplate } from '/src/assets/js/utils.js'
+import fixtures from './fixtures/teaser_poster.json'
+
 import teaser from './teaser_poster.hbs'
 import teserPosterLg from './fixtures/teaser_poster_lg.json'
 import teserPosterMd from './fixtures/teaser_poster_md.json'
 import teserPosterMdLabel from './fixtures/teaser_poster_md_label.json'
 import teserPosterMdLabelByline from './fixtures/teaser_poster_md_label_byline.json'
 
-const Template = (args, { globals: { customConditionalToolbar } }) => {
-    // You can either use a function to create DOM elements or use a plain html string!
-    // return `<div>${label}</div>`;
-    let brand =
-        undefined !== customConditionalToolbar ? customConditionalToolbar['brands'] : 'hessenschau'
-    return teaser({ brand, ...args })
+const handlebars = require('hrHandlebars')
+const hbsTemplates = []
+hbsTemplates['default'] = handlebars.compile(`
+    {{> components/teaser/teaser_poster }}   
+  `)
+
+
+const Template = (args) => {
+    return hbsTemplates['default']({ ...args })
+}
+
+const snapshotTemplate = (args) => {
+    return getSnapshotsTemplate({ hbsTemplates, args })
 }
 
 export default {
@@ -20,6 +30,7 @@ export default {
 
         chromatic: {
             diffThreshold: 0.3,
+            disableSnapshot: true
         },
     },
 
@@ -42,9 +53,9 @@ export default {
 
     decorators: [
         (Story) => {
-            return `<div class="grid grid-page"><div class="grid grid-cols-12 py-6 col-full gap-x-6 gap-y-6 sm:px-9.5 sm:col-main">  
+            return `<div class="grid grid-page">  
              ${Story()} 
-             </div></div>`
+             </div>`
         },
     ],
 }
@@ -52,23 +63,87 @@ export default {
 export const Poster50 = {
     render: Template.bind({}),
     name: 'Poster 50',
-    args: teserPosterLg.logicItem.includeModel,
+    decorators: [
+        (Story) => {
+            return `<div class="grid grid-cols-12 py-6 col-full gap-x-6 gap-y-6 sm:px-9.5 sm:col-main">  
+             ${Story()} 
+             </div>`
+        },
+    ],
+    args: fixtures.group_50["50"].args.logicItem.includeModel,
+}
+
+export const Poster50MitLabel = {
+    render: Template.bind({}),
+    name: 'Poster 50 mit Label',
+    decorators: [
+        (Story) => {
+            return `<div class="grid grid-cols-12 py-6 col-full gap-x-6 gap-y-6 sm:px-9.5 sm:col-main">  
+             ${Story()} 
+             </div>`
+        },
+    ],
+    args: fixtures.group_50["50_with_label"].args.logicItem.includeModel,
+}
+
+export const Poster50MitLabelByline = {
+    render: Template.bind({}),
+    name: 'Poster 50 mit Label und Byline',
+    decorators: [
+        (Story) => {
+            return `<div class="grid grid-cols-12 py-6 col-full gap-x-6 gap-y-6 sm:px-9.5 sm:col-main">  
+             ${Story()} 
+             </div>`
+        },
+    ],
+    args: fixtures.group_50["50_with_label_and_byline"].args.logicItem.includeModel,
 }
 
 export const Poster33 = {
     render: Template.bind({}),
     name: 'Poster 33',
-    args: teserPosterMd.logicItem.includeModel,
+    decorators: [
+        (Story) => {
+            return `<div class="grid grid-cols-12 py-6 col-full gap-x-6 gap-y-6 sm:px-9.5 sm:col-main">  
+             ${Story()} 
+             </div>`
+        },
+    ],
+    args: fixtures.group_33["33"].args.logicItem.includeModel,
 }
 
 export const Poster33MitLabel = {
     render: Template.bind({}),
     name: 'Poster 33 mit Label',
-    args: teserPosterMdLabel.logicItem.includeModel,
+    decorators: [
+        (Story) => {
+            return `<div class="grid grid-cols-12 py-6 col-full gap-x-6 gap-y-6 sm:px-9.5 sm:col-main">  
+             ${Story()} 
+             </div>`
+        },
+    ],
+    args: fixtures.group_33["33_with_label"].args.logicItem.includeModel,
 }
 
 export const Poster33MitLabelByline = {
     render: Template.bind({}),
     name: 'Poster 33 mit Label und Byline',
-    args: teserPosterMdLabelByline.logicItem.includeModel,
+    decorators: [
+        (Story) => {
+            return `<div class="grid grid-cols-12 py-6 col-full gap-x-6 gap-y-6 sm:px-9.5 sm:col-main">  
+             ${Story()} 
+             </div>`
+        },
+    ],
+    args: fixtures.group_33["33_with_label_and_byline"].args.logicItem.includeModel,
+}
+
+export const Snapshot = {
+    render: snapshotTemplate.bind({}),
+    name: 'Snapshot',
+
+    args: fixtures,
+    parameters: {
+        chromatic: { disableSnapshot: false },
+    }
 }
