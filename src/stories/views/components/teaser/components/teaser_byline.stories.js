caee0b0f6f91dc4248ed53dcca9ed9f78a408a7d
Stefan Zülch <stefan.zuelch@hr.de>
2024-09-17 10:09:42 +0200

    :sparkles: Add snapshot story to teaser byline component

--- src/stories/views/components/teaser/components/teaser_byline.stories.js
+++ src/stories/views/components/teaser/components/teaser_byline.stories.js
@@ -1,28 +1,54 @@
-import byline from './teaser_byline.hbs'
-import teaserStandard from '../fixtures/teaser_standard.json'
+import { getSnapshotsTemplate } from '/src/assets/js/utils.js'
+import fixtures from 'components/teaser/components/fixtures/teaser_byline.json'
+
+const handlebars = require('hrHandlebars')
+const hbsTemplates = []
+hbsTemplates['default'] = handlebars.compile(`
+        {{> components/teaser/components/teaser_byline}} 
+  `)
 
 const Template = ({ ...args }) => {
-    return byline({ ...args })
+    return hbsTemplates['default']({ ...args })
+}
+
+const snapshotTemplate = (args) => {
+    return getSnapshotsTemplate({ hbsTemplates, args })
 }
 
 export default {
     title: 'Komponenten/Teaser/Komponenten/Byline',
+    parameters: {
+        chromatic: {
+            diffThreshold: 0.3,
+            disableSnapshot: true
+        },
+    },
 }
 
 export const Default = {
     render: Template.bind({}),
     name: 'default',
-    args: teaserStandard.group_hero.hero.args.logicItem.includeModel,
+    args: fixtures.byline_with_teaser_info.args.logicItem.includeModel,
 }
 
 export const WithComments = {
     render: Template.bind({}),
     name: 'with comments',
-    args: teaserStandard.group_hero.hero_with_comments.args.logicItem.includeModel,
+    args: fixtures.byline_with_comments_and_teaser_info.args.logicItem.includeModel,
 }
 
 export const CommentsOnly = {
     render: Template.bind({}),
     name: 'comments only',
-    args: teaserStandard.group_hero.hero_with_comments_and_without_teaser_info.args.logicItem.includeModel,
+    args: fixtures.byline_with_comments.args.logicItem.includeModel,
+}
+
+export const Snapshot = {
+    render: snapshotTemplate.bind({}),
+    name: 'Snapshot',
+
+    args: fixtures,
+    parameters: {
+        chromatic: { disableSnapshot: false },
+    }
 }
