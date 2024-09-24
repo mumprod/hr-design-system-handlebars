53c3348ab024d6606df48e25b8cc73bf4490cb7f
selbaciri <saad.elbaciri@hr.de>
2024-09-24 15:05:16 +0200

    DPE-3306. add checkbox field

---
+++ src/stories/views/components/forms/form_select.stories.js
@@ -0,0 +1,34 @@
+
+import selectJson from './fixtures/form_select.json'
+
+const handlebars = require('hrHandlebars')
+
+
+export default {
+    title: 'Komponenten/Formulare',
+    decorators: [
+        (Story) => {
+            return `<div class="grid grid-page">
+                        <div class="grid bg-white grid-article">
+                            ${Story()}
+                        </div>
+                    </div>`
+        },
+    ],
+}
+const Template = (args) => {
+    let hbsTemplate = handlebars.compile(`
+    {{#>components/forms/backgroundBox  }}  
+            <form class="relative flex flex-col justify-center overflow-hidden group" id="form--{{nextRandom}}" action="{{this.url}}" method="post" enctype="{{if this.isMultipart 'multipart/form-data' 'application/x-www-form-urlencoded'}}" accept-charset="utf-8" >     
+                {{> components/forms/fields }}
+            </form>
+    {{/components/forms/backgroundBox }}
+  `)
+    return hbsTemplate({ ...args })
+}
+
+export const Select = {
+    render: Template.bind({}),
+    name: 'Select',
+    args: selectJson,
+}
