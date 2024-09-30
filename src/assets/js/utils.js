9c1a80cf18be42109d7dadf74e43113379dcefd5
Stefan Zülch <stefan.zuelch@hr.de>
2024-09-30 14:41:36 +0200

    :recycle: remove console outputs

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -8,7 +8,6 @@
  */
 const getSnapshotsTemplate = (args) => {
     const { hbsTemplates, args: snapshotsJson } = args
-    console.log("SnapshotsJson First", snapshotsJson)
     const config = snapshotsJson.config || {}
     const renderAsGrid = undefined !== config.layout && "grid" === config.layout
     const renderSnapshotsWrapper = undefined !== config.renderSnapshotsWrapper ? config.renderSnapshotsWrapper : true
@@ -31,7 +30,6 @@ const getSnapshotsTemplate = (args) => {
         })
         template += snapshotsWrapperEnd
     } else {
-        console.log("SnapshotsJson", snapshotsJson)
         const filteredSnapshots = Object.entries(snapshotsJson).filter(([key, value]) =>
             key !== "config" && (value.config === undefined || value.config.useAsSnapshot !== false)
         )
@@ -44,7 +42,6 @@ const getSnapshotsTemplate = (args) => {
 
 const renderSnapshots = (snapshots, hbsTemplates, defaultTemplate, defaultPath, renderSnapshotsWrapper) => {
     let template = ''
-    console.log("Snapshots", snapshots)
     snapshots.forEach(([key, value]) => {
         const snapshotItemConfig = value.config || {}
         const renderSnapshotItemWrapper = undefined !== snapshotItemConfig.renderSnapshotItemWrapper ? snapshotItemConfig.renderSnapshotItemWrapper : true
