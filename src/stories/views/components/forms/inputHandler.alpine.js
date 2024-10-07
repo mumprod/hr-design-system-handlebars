62af30e11b4d58c926ce9b54a135c5ca929769c2
vascoeduardo <v.m.p.eduardo@googlemail.com>
2024-10-07 17:19:14 +0200

    refactored inputHandler.alpine.js. It handles all input fields now

--- src/stories/views/components/forms/inputHandler.alpine.js
+++ src/stories/views/components/forms/inputHandler.alpine.js
@@ -1,14 +1,16 @@
-export default function inputHandler(element, errorMandatory, type, errorEmail, prefilledText = '') {
+export default function inputHandler(element, formId, errorMandatory, type, errorEmail, prefilledText = '') {
     return { 
         [element]: prefilledText,
-        valid: true, 
+        valid: false, 
         wasFocused: false, 
         isFocused: false,
-        validEmail: false,
         isChecked: false,
+        typeMismatch: true,
+        valueMissing: true,
+
         errorMessage() { 
             if( type == "email"){
-                return !this.valid ? errorMandatory : errorEmail
+                return this.valueMissing ? errorMandatory : errorEmail
             } 
             else {
                 return errorMandatory
@@ -17,19 +19,23 @@ export default function inputHandler(element, errorMandatory, type, errorEmail, 
         hideDescription() {
             switch (type) {
                 case "email":
-                    return Boolean((!this.valid && this.wasFocused && !this.isFocused) || (!this.validEmail && this.wasFocused && !this.isFocused));
+                    return Boolean((!this.valid && this.wasFocused && !this.isFocused) || (this.typeMismatch && this.wasFocused && !this.isFocused) || (!this.valid && !this.isFocused && this.$store.forms.submissionAttempted[formId]));
                 case "checkbox":
-                    return Boolean(!this.valid && this.wasFocused && !this.isFocused && !this.isChecked);
+                    return Boolean(!this.valid && this.wasFocused && !this.isFocused && !this.isChecked || (!this.valid && !this.isFocused && this.$store.forms.submissionAttempted[formId]));
+                case "select":
+                    return Boolean((!this.valid && this.wasFocused && !this.isFocused) || (!this.valid && !this.isFocused && this.$store.forms.submissionAttempted[formId]))
                 default:
-                    return Boolean(!this.valid && this.wasFocused && !this.isFocused);
+                    return Boolean((!this.valid && this.wasFocused && !this.isFocused) || (!this.valid && !this.isFocused && this.$store.forms.submissionAttempted[formId]));
             }
         },
         hideError() { 
-                return Boolean(!this.hideDescription())  
+            return Boolean(!this.hideDescription())  
         },
-        validateEmail() {
-            var emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-            this.validEmail = emailRegex.test( this[element]);  
-        }  
+        validateField() {
+            var field = document.getElementById(element)  
+            this.typeMismatch = field.validity.typeMismatch;
+            this.valueMissing  = field.validity.valueMissing;
+            this.valid = field.checkValidity()
+        }
     };
 }
